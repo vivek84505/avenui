66ec7b701ee154232ed04a381a4909731f35607f
Vivek <vivk84505@gmail.com>
2020-12-03 22:42:33 +0530

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -14,22 +14,11 @@
     //     $config['smtp_pass'] = $CI->db->get_where('general_settings',array('type'=>'smtp_pass'))->row()->value;
     // }
 
-
-    $config['useragent'] = 'CodeIgniter';
-    $config['protocol'] = 'smtp';
-    //$config['mailpath'] = '/usr/sbin/sendmail';
-    $config['smtp_host'] = 'ssl://mail.avenui.net';
-    $config['smtp_user'] = 'vivektest@avenui.net';
-    $config['smtp_pass'] = '~Hu(PNoa!5-P';
-    $config['smtp_port'] = 465; 
-    $config['smtp_timeout'] = 5;
-    $config['wordwrap'] = TRUE;
-    $config['wrapchars'] = 76;
-    $config['mailtype'] = 'text/plain';
-    $config['charset'] = 'utf-8';
-    $config['validate'] = FALSE;
-    $config['priority'] = 3;
-    $config['crlf'] = "\r\n";
-    $config['newline'] = "\r\n";
-    $config['bcc_batch_mode'] = FALSE;
-    $config['bcc_batch_size'] = 200;
+    $configs = array(
+    'protocol'  =>  'smtp',
+    'smtp_host' =>  'ssl://mail.avenui.net',
+    'smtp_user' =>  'vivektest@avenui.net',
+    'smtp_pass' =>  '~Hu(PNoa!5-P',
+    'smtp_port' =>  '465'
+);
+    
